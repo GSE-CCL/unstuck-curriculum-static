32166b6a9761f7c6a2fe82fb19e987716c516c67
Jeff Hawson <jhawson@gmail.com>
2022-09-05 08:52:55 -0400

    Update scrape to use wget2 for static images

--- scrape.sh
+++ scrape.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
-SITE=https://ccl-unstuck-curriculum-amye4qzbia-uc.a.run.app
-EXTRA=https://ccl-unstuck-curriculum-amye4qzbia-uc.a.run.app/strategies.html
+SITE=ccl-unstuck-curriculum-amye4qzbia-uc.a.run.app
 
 # delete existing site files
 rm *.html
-rm -rf static
+rm -rf static modules translations
 
 # scrape dynamic site
-wget --mirror --convert-links --adjust-extension --page-requisites --no-parent --no-host-directories $SITE
-wget --mirror --convert-links --adjust-extension --page-requisites --no-parent --no-host-directories $EXTRA
+wget --mirror --convert-links --adjust-extension --page-requisites --no-parent --no-host-directories https://$SITE
+wget --mirror --convert-links --adjust-extension --page-requisites --no-parent --no-host-directories https://$EXTRA/strategies.html
+
+# use wget2 to scrape extra media-width-specific files; wget2 appears to have bugs/limitations
+# that prevent it from being generally useful, but it's helpful for this one task
+# `sudo apt get wget2` (if needed)
+wget2 --mirror --convert-links --adjust-extension --page-requisites --no-parent $SITE
+cp -rf $SITE/static/img/* static/img/
+rm -rf $SITE

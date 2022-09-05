0720dd5d9e7c37699b39f1e36983c08edd9a77d6
Jeff Hawson <jhawson@gmail.com>
2022-09-05 09:09:13 -0400

    Fix bugs found in PK review

--- scrape.sh
+++ scrape.sh
@@ -8,11 +8,11 @@ rm -rf static modules translations
 
 # scrape dynamic site
 wget --mirror --convert-links --adjust-extension --page-requisites --no-parent --no-host-directories https://$SITE
-wget --mirror --convert-links --adjust-extension --page-requisites --no-parent --no-host-directories https://$EXTRA/strategies.html
+wget --mirror --convert-links --adjust-extension --page-requisites --no-parent --no-host-directories https://$SITE/strategies.html
 
 # use wget2 to scrape extra media-width-specific files; wget2 appears to have bugs/limitations
 # that prevent it from being generally useful, but it's helpful for this one task
 # `sudo apt get wget2` (if needed)
-wget2 --mirror --convert-links --adjust-extension --page-requisites --no-parent $SITE
+wget2 --mirror --convert-links --adjust-extension --page-requisites --no-parent https://$SITE
 cp -rf $SITE/static/img/* static/img/
 rm -rf $SITE
